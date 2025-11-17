7dff07643de211de1036eb78a749de8928cd18d0
1ucho <lcojpe@gmail.com>
2025-11-17 18:12:59 -0300

    Add files via upload

---
+++ assets/estilos.css
@@ -0,0 +1,8 @@
+document.getElementById("navbar").innerHTML = `
+<nav>
+  <a href="/index.html">🏠 Inicio</a>
+  <a href="/talleres.html">📚 Talleres</a>
+  <a href="/dashboard.html">📊 Dashboard</a>
+  <a href="/politica-privacidad.html">🔒 Privacidad</a>
+</nav>
+`;
